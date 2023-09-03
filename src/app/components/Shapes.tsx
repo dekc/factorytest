e7c4793d0832b3050d19a49ee557327abe4710f3
dekc <dekc@outlook.com>
2023-09-03 23:49:07 +1000

    Playing with CSS

--- src/app/components/Shapes.tsx
+++ src/app/components/Shapes.tsx
@@ -15,6 +15,11 @@ const ChildContainer = styled.div`
   border-radius: 0.5rem;
   background-color: #f5f5f5;
   box-shadow: 0 0 0.5rem #ccc;
+  // clip-path: circle(25% at 100% 0%);
+  // transition: clip-path 0.3s ease-in-out;
+  // &:hover {
+  //   clip-path: circle(200% at 100% 0%);
+  // }
 }`;
 
 const CenteredTextDiv = styled.div`
@@ -22,6 +27,13 @@ const CenteredTextDiv = styled.div`
   justify-content: center; // Horizontally center the content
   align-items: center; // Vertically center the content
   text-align: center; // Ensure text is centered
+`;
+const ErrorContainer = styled.div``;
+
+const ErrorSpan = styled.span`
+  color: darkgray;
+  cursor: ;
+  font-size: 0.8rem;
 `;
 
 const ErrorDiv = styled.div`
@@ -31,27 +43,45 @@ const ErrorDiv = styled.div`
   margin: 1rem;
   border-radius: 0.5rem;
   background-color: #f5151510;
-  filter: blur(5px);
-  transition: filter 0.3s ease-in-out;
-  &:hover {
-    filter: none;
-  }
+  box-shadow: 0 0 0.5rem #751515;
+  // clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+  clip-path: ${(props) =>
+    props.isHovered
+      ? 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)'
+      : 'polygon(0 0, 0% 0%, 0% 100%, 0% 100%)'};
+  filter: ${(props) => (props.isHovered ? 'blur(0px)' : 'blur(5px)')};
+  transition: filter 0.3s ease-in-out 0.3s, clip-path 0.5s ease-in-out;
+`;
+
+const ErrorTitle = styled.h3`
+  color: navy;
 `;
 
 const ConfigError = ({ errors }: { errors: string[] | null }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   if (!errors || errors.length === 0) {
     return null;
   }
 
   return (
-    <>
-      <h3>There was an error processing the config</h3>
-      <ErrorDiv>
+    <ErrorContainer>
+      <ErrorTitle>
+        There was an error processing the config
+        <ErrorSpan
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {' '}
+          (hover for details)
+        </ErrorSpan>
+      </ErrorTitle>
+      <ErrorDiv isHovered={isHovered}>
         {errors.map((error: unknown, index: number) => (
           <p key={`${index}_`}>{error as string}</p>
         ))}
       </ErrorDiv>
-    </>
+    </ErrorContainer>
   );
 };
 
